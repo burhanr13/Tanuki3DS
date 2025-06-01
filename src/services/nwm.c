f5089f617baf24fc708285225761abbd063ba0a4
Burhan Rajgara <burhan.rajgara@gmail.com>
2025-06-01 15:10:05 -0700

    add svc 29

--- src/services/nwm.c
+++ src/services/nwm.c
@@ -14,7 +14,7 @@ DECL_PORT(nwm_uds) {
             lwarn("unknown command 0x%04x (%x,%x,%x,%x,%x)", cmd.command,
                   cmdbuf[1], cmdbuf[2], cmdbuf[3], cmdbuf[4], cmdbuf[5]);
             cmdbuf[0] = IPCHDR(1, 0);
-            cmdbuf[1] = -1;
+            cmdbuf[1] = 0;
             break;
     }
 }
